4cb54d653aac28411152f5a334bcca1452833eeb
iwan933 <johannboschmann@hotmail.de>
2015-10-27 00:52:32 +0100

    +added util folder +added set class +updated todo.txt

---
+++ Python/lecture/Set.py
@@ -0,0 +1,87 @@
+class Set(Object):
+
+	def __init__(self, data_list=None, *args):
+		self.data = []
+		
+		for arg in args:
+			self.insert(data, arg)
+
+		self.data.sort()
+
+	def __add__(self, other):
+		result = []
+		if isinstance(other, Set):
+			for x in other.data:
+				self.insert(result, x)
+		else:
+			self.insert(result, other)
+
+		for x in self.data:
+			self.insert(result, x)
+
+		return Set(data_list=result)
+
+	def __mul__(self, other):
+		assert isinstance(other, Set)
+
+		result = []
+
+		for y in self.data:
+			if y in other.data:
+				self.insert(result, y)
+
+		return Set(data_list=result)
+
+	def __str__(self):
+		result = ''
+
+		for x in self.data:
+			result += ', %s' % str(x)
+
+		return result
+
+	def __pow__(self, other):
+		assert other == 2
+
+		return self.cartesian()
+		
+
+	def __rpow__(self, other):
+
+		assert other == 2
+
+		return self.cartesian()
+
+	def __div__(self, other):
+		assert isinstance(other, Set)
+
+		result = []
+
+		for x in self.data:
+			if x not in other.data:
+				self.insert(result, x)
+
+		return Set(data_list=result)
+
+	def __mod__(self, other):
+		assert isinstance(other, Set)
+
+		return (self / other) + (other / self)
+
+	# helper method to only insert elements into an array 'arr', that are
+	# not present in the array (set behavior)
+	def insert(self, arr, value):
+		if value in arr:
+			return
+
+		arr.add(value)
+
+	# returns a new set representing the cartesian product of the current
+	# set
+	def cartesian(self):
+		result = []
+		for x in self.data:
+			for y in self.data:
+				self.insert(result, (x,y))
+
+		return Set(data_list=result)
--- Python/lecture/util/__init__.py
+++ Python/lecture/util/__init__.py
@@ -1,5 +1,5 @@
 
 
 __all__ = [
-	'Set'
+	'is_prime'
 ]
